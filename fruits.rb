b16326b4eb48e47d45375196f3b4fd8e9463b840
Luke Bartel <bartelluke@gmail.com>
2014-06-17 13:14:14 -0600

    Finish minimum stories

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,23 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+puts fruits.length
+
+puts fruits.index('bananas')
+
+print fruits.insert(1, 'raspberries')
+
+puts
+
+fruits.each do |f|
+   puts f.length
+end
+
+fruits.each do |f|
+  puts f.upcase
+end
+
+puts
+
+fruits.each do |f|
+  puts f if f.include? 'g'
+end
