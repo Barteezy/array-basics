f5ab4e007c5da9ed5566f630775231b9d833251c
Luke Bartel <bartelluke@gmail.com>
2014-06-23 07:24:26 -0700

    added solutions to friends.rb

--- friends.rb
+++ friends.rb
@@ -1,19 +1,39 @@
-puts "Please tell me your friends' names"
+require 'pp'
+
+puts "gimme yo friends names"
+
 
 fnames = []
 
 while true
-
   fname = gets.chomp
 
-  if fname == ''
-
+  if fname == ""
     break
-
   end
 
   fnames.push fname
-
 end
 
+pp "Here are your friends"
+
 print fnames
+
+pp "Here they are in alphabetical order."
+
+pp fnames.sort
+
+pp "Here they are in reverse order."
+
+pp fnames.reverse
+
+pp "Here they are in reverse alphabetical order."
+
+pp fnames.sort.reverse
+
+new_array = []
+fnames.each do |name|
+  new_array << [name, name.length]
+end
+pp new_array
+
